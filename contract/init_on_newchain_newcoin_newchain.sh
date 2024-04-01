de12bf4b4bc09d75c7e82ed1bac49f790c370ef2
eddyguo <eddy@eddydeMacBook-Pro.local>
2024-04-01 14:26:54 +0800

    +1

--- contract/init_on_newchain_newcoin_newchain.sh
+++ contract/init_on_newchain_newcoin_newchain.sh
@@ -4,11 +4,11 @@ set -xv
 relayer_test=b0cd4ec0ef9382a7ca42c8a68d8d250c70c1bead7c004d8d78aa00c5a3cef7f7
 relayer_local=83a666efeed6ffd0bc54c30ad1d1b904e8e49608a7298138a85ea428ce15b902
 
-##create ca
+##create carelayer_testu
 near --nodeUrl  http://120.232.251.101:3030 create-account multi_sig7_test.node0 --publicKey CuAL8qaTLg3nMQ3Jz3B2yq6SYCSygGoR2q5nEACHxVyY  --masterAccount node0 --initialBalance 100 --keyPath ~/.near-credentials/local/node0.json
 near --nodeUrl  http://120.232.251.101:3030 create-account multi_sig7_local.node0 --publicKey 9ruaNCMS1BvXfWT6MySeveTXrn2fLekbVCaWwETL18ZP  --masterAccount node0 --initialBalance 100 --keyPath ~/.near-credentials/local/node0.json
-near --nodeUrl  http://120.232.251.101:3030 call usdt   ft_transfer '{"receiver_id":"multi_sig7_test.node0","amount":"1000000000000000000000"}'  --accountId node0 --keyPath ~/.near-credentials/local/node0.json --gas 600000000000000
-near --nodeUrl  http://120.232.251.101:3030 call usdt   ft_transfer '{"receiver_id":"multi_sig7_local.node0","amount":"1000000000000000000000"}'  --accountId node0 --keyPath ~/.near-credentials/local/node0.json --gas 600000000000000
+near --nodeUrl  http://120.232.251.101:3030 call usdt   ft_transfer '{"receiver_id":"'$relayer_test'","amount":"1000000000000000000000"}'  --accountId node0 --keyPath ~/.near-credentials/local/node0.json --gas 600000000000000
+near --nodeUrl  http://120.232.251.101:3030 call usdt   ft_transfer '{"receiver_id":"'$relayer_local'","amount":"1000000000000000000000"}'  --accountId node0 --keyPath ~/.near-credentials/local/node0.json --gas 600000000000000
 
 ##deploy ca
 near --nodeUrl  http://120.232.251.101:3030 deploy multi_sig7_test.node0 ./target/wasm32-unknown-unknown/release/contract.wasm  --keyPath ./multi_sig7_test.node0.json 
@@ -19,4 +19,7 @@ coins=("btc" "eth" "usdt" "usdc" "cly" "dw20")
 for coin in "${coins[@]}"; do
     near --nodeUrl http://120.232.251.101:3030 call $coin  ft_transfer '{"receiver_id":"'$relayer_local'","amount":"100000000000000000000000"}' --accountId node0 --keyPath ../test_coin/coin_key/node0.json --gas 600000000000000
     near --nodeUrl http://120.232.251.101:3030 view $coin ft_balance_of '{"account_id":"'$relayer_local'"}'
+
+    near --nodeUrl http://120.232.251.101:3030 call $coin  ft_transfer '{"receiver_id":"'$relayer_test'","amount":"100000000000000000000000"}' --accountId node0 --keyPath ../test_coin/coin_key/node0.json --gas 600000000000000
+    near --nodeUrl http://120.232.251.101:3030 view $coin ft_balance_of '{"account_id":"'$relayer_test'"}'
 done
